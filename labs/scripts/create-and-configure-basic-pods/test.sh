3b9f99969b25720211e1cb337810c93639a94222
seaneon <sean.r.barbour@gmail.com>
2022-05-12 16:51:33 -0400

    15 was not long enough for pod ready

--- labs/scripts/create-and-configure-basic-pods/test.sh
+++ labs/scripts/create-and-configure-basic-pods/test.sh
@@ -3,7 +3,7 @@
 kubectl config use-context kubernetes-the-alta3-way
 kubectl create -f ~/mycode/yaml/simpleservice.yaml
 kubectl apply -f ~/mycode/yaml/webby-pod01.yaml
-sleep 15
+sleep 35
 kubectl get pods
 kubectl delete -f ~/mycode/yaml/simpleservice.yaml
 kubectl delete -f ~/mycode/yaml/webby-pod01.yaml
