ac9777fe8330326f4255f3053a8b096f067f441e
seaneon <sean.r.barbour@gmail.com>
2022-05-12 16:24:10 -0400

    setups

--- labs/scripts/create-and-configure-basic-pods/teardown.sh
+++ labs/scripts/create-and-configure-basic-pods/teardown.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
 
+kubectl delete -f ~/mycode/yaml/simpleservice.yaml
+kubectl delete -f ~/mycode/yaml/webby-pod01.yaml
--- labs/scripts/create-and-configure-basic-pods/test.sh
+++ labs/scripts/create-and-configure-basic-pods/test.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
 kubectl config use-context kubernetes-the-alta3-way
+kubectl create -f ~/mycode/yaml/simpleservice.yaml
+kubectl apply -f ~/mycode/yaml/webby-pod01.yaml
+sleep 15
+kubectl get pods
+kubectl delete -f ~/mycode/yaml/simpleservice.yaml
+kubectl delete -f ~/mycode/yaml/webby-pod01.yaml
 
