26df00768979c17d2efd47817a1fee7a5263db4c
seaneon <sean.r.barbour@gmail.com>
2022-05-13 12:33:03 -0400

    td and test

--- labs/scripts/admission-controller/teardown.sh
+++ labs/scripts/admission-controller/teardown.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
+kubectl delete pod no-lr
+kubectl delete -f ~/mycode/yaml/lim-ran.yml
 
--- labs/scripts/admission-controller/test.sh
+++ labs/scripts/admission-controller/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-
+kubectl run no-lr --image=nginx:1.19.6
+kubectl apply -f ~/mycode/yaml/lim-ran.yml
