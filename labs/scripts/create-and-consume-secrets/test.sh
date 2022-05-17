2afa38f2d8288d3c1892bc8d12bdee6af5e9f7f1
seaneon <sean.r.barbour@gmail.com>
2022-05-17 15:15:52 -0400

    still working on secrets setup

--- labs/scripts/create-and-consume-secrets/test.sh
+++ labs/scripts/create-and-consume-secrets/test.sh
@@ -5,7 +5,7 @@ openssl req -new -key webby.com.key -out webby.com.key.csr -subj "/C=US/ST=PA/L=
 openssl x509 -req -in  webby.com.key.csr -CA ~/k8s-certs/ca.pem -CAkey ~/k8s-certs/ca-key.pem -CAcreateserial -out webby.com.crt -days 1825 -sha256
 kubectl create secret generic webby-keys --from-file=/home/student/k8s-certs/webby.com.crt --from-file=/home/student/k8s-certs/webby.com.key
 kubectl delete cm nginx-conf
-kubectl create cm nginx-conf --from-file=~/mycode/config/nginx.conf.createandconsumesecrets
+kubectl create cm nginx-conf --from-file=/home/student/mycode/config/nginx.conf.createandconsumesecrets
 kubectl apply -f ~/mycode/yaml/nginx-locked-n-loaded-02.yaml
 sleep 15
 curl https://localhost:9090
