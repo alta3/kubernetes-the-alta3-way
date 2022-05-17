1ef771f7f04bd5629fec464b96c1c87e554c09cd
seaneon <sean.r.barbour@gmail.com>
2022-05-17 16:13:28 -0400

    setup and test

--- labs/scripts/multi-container-pod-design/setup.sh
+++ labs/scripts/multi-container-pod-design/setup.sh
@@ -2,4 +2,17 @@
 
 echo STARTING ----------
 kubectl get nodes || printf "\n\n Looks like K8s may not be installed?\n\n"
+kubectl apply -f ~/mycode/yaml/prod-ns.yaml -f ~/mycode/yaml/test-ns.yaml -f ~/mycode/yaml/dev-ns.yaml
+kubectl create configmap nginx-conf --from-file=/home/student/mycode/config/nginx.conf
+kubectl create configmap index-file --from-file=/home/student/mycode/config/index.html
+cd ~/k8s-certs
+openssl genrsa -out webby.com.key 2048
+openssl req -new -key webby.com.key -out webby.com.key.csr -subj "/C=US/ST=PA/L=Harrisbug/O=webby/OU=webby/CN=localhost"
+openssl x509 -req -in  webby.com.key.csr -CA ~/k8s-certs/ca.pem -CAkey ~/k8s-certs/ca-key.pem -CAcreateserial -out webby.com.crt -days 1825 -sha256
+kubectl create secret generic webby-keys --from-file=/home/student/k8s-certs/webby.com.crt --from-file=/home/student/k8s-certs/webby.com.key
+echo "It was a bright cold day in April, and the clocks were striking thirteen." >> /home/student/nginx.txt
+kubectl create configmap nginx-txt --from-file=/home/student/nginx.txt
+kubectl delete pod nginx
+sleep 30
+kubectl get pod nginx-configured
 echo END OF SETUP ----------
--- labs/scripts/multi-container-pod-design/test.sh
+++ labs/scripts/multi-container-pod-design/test.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
+kubectl apply -f ~/mycode/yaml/netgrabber.yaml
+kubectl exec netgrab -c busybox -- sh "ping 8.8.8.8 -c 10"
+sleep 10
+kubectl delete pod netgrab
+kubectl delete configmap nginx-conf
+kubectl create -f ~/mycode/yaml/nginx-conf.yaml
+kubectl create -f ~/mycode/yaml/webby-nginx-combo.yaml
+sudo rm /etc/nginx/sites-enabled/fruit-ingress
+sudo nginx -s reload
 
