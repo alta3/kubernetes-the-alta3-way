892e2b7f6b159d56402013b01bae3dd43095c203
bryfry <bryon.fryer@gmail.com>
2022-05-21 21:06:56 +0000

    full test

--- labs/setup/examining-resources-with-kubectl-describe/teardown.sh
+++ labs/setup/examining-resources-with-kubectl-describe/teardown.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-# kubectl delete pod webweb
-# kubectl delete -f ~/mycode/yaml/webweb-deploy.yaml
 kubectl delete pod webweb --now
 kubectl delete -f ../yaml/webweb-deploy.yaml
--- labs/setup/examining-resources-with-kubectl-describe/test.sh
+++ labs/setup/examining-resources-with-kubectl-describe/test.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 kubectl run --port=8888 --image=registry.gitlab.com/alta3research/webby webweb
 kubectl wait --for condition=Ready --timeout 60s pod/webweb
+kubectl delete pod webweb --now
 kubectl apply -f ../yaml/webweb-deploy.yaml
 kubectl wait --for condition=Available --timeout 60s deployment.apps/webweb
