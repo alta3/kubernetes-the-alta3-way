225a6d62818e871daf74eae21fa5b5de78b5c3d3
bryfry <bryon@fryer.io>
2022-05-21 15:06:09 +0000

    fixup setup

--- labs/setup/examining-resources-with-kubectl-describe/setup.sh
+++ labs/setup/examining-resources-with-kubectl-describe/setup.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -euo pipefail
 
-echo STARTING ----------
-kubectl get nodes || printf "\n\n Looks like K8s may not be installed?\n\n"
 kubectl config use-context kubernetes-the-alta3-way
-echo END OF SETUP ----------
