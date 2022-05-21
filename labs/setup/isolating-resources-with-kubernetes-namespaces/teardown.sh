7917c00e4f9fa78b25e0a055a5bfee051c629b9e
bryfry <bryon.fryer@gmail.com>
2022-05-21 23:23:07 +0000

    s,t,t updates

--- labs/setup/isolating-resources-with-kubernetes-namespaces/teardown.sh
+++ labs/setup/isolating-resources-with-kubernetes-namespaces/teardown.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-kubectl delete -f ~/mycode/yaml/test-ns.yaml
-kubectl delete -f ~/mycode/yaml/dev-ns.yaml
-kubectl delete -f ~/mycode/yaml/prod-ns.yaml
-kubectl delete -f ~/mycode/yaml/test-rq.yaml --namespace=test
-kubectl delete -f ~/mycode/yaml/dev-rq.yaml --namespace=dev
-kubectl delete -f ~/mycode/yaml/prod-rq.yaml --namespace=prod
+kubectl delete -f ../yaml/test-rq.yaml --namespace=test
+kubectl delete -f ../yaml/dev-rq.yaml --namespace=dev
+kubectl delete -f ../yaml/prod-rq.yaml --namespace=prod
+kubectl delete -f ../yaml/test-ns.yaml
+kubectl delete -f ../yaml/dev-ns.yaml
+kubectl delete -f ../yaml/prod-ns.yaml
