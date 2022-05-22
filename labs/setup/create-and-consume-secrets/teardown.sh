fe180c3480ca7ee898373b4f4e31426211ed6399
Stuart Feeser <sfeeser@gmail.com>
2022-05-22 14:13:21 -0400

    Update teardown.sh

--- labs/setup/create-and-consume-secrets/teardown.sh
+++ labs/setup/create-and-consume-secrets/teardown.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-kubectl delete --ignore-not-found --wait=false \
-  -f ../yaml/prod-ns.yaml \
-  -f ../yaml/test-ns.yaml \
-  -f ../yaml/dev-ns.yaml
-
-kubectl delete --ignore-not-found --wait=false secret generic webby-keys
-kubectl delete --ignore-not-found --wait=false configmap nginx-txt
-kubectl delete --ignore-not-found --wait=false configmap nginx-conf
-kubectl delete --ignore-not-found --wait=false configmap index-file
-kubectl delete --ignore-not-found --wait=false -f ../yaml/nginx-configured.yaml
-kubectl delete --ignore-not-found --wait=false -f ../yaml/nginx-locked-n-loaded-02.yaml
+kubectl delete --ignore-not-found --wait=false -f ../yaml/mysql-secret.yaml
+kubectl delete --ignore-not-found --wait=false -f ../yaml/mysql-locked.yaml
