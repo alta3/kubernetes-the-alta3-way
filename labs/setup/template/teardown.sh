64b6c3d571f4f1c00ee7b394f33364958a552014
bryfry <bryon.fryer@gmail.com>
2022-05-22 19:54:59 +0000

    s,t,t updates

---
+++ labs/setup/template/teardown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# NOTE: ${WAIT} is exported from the parent script, do not hard code it
+#kubectl delete --ignore-not-found --wait="${WAIT}" \
+#  -f ../yaml/example1.yaml \
+#  -f ../yaml/example2.yaml \
+#  -f ../yaml/example3.yaml \
+
+#kubectl delete --ignore-not-found --wait="${WAIT}" pod not-created-by-a-manifest
+#kubectl delete --ignore-not-found --wait="${WAIT}" -f ../yaml/yaml-in-a-namespace.yaml --namespace=test
---
+++ labs/setup/template/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xeuo pipefail
+# NOTE: -x shows testing commands
+
+#kubectl run a-pod-worth-waiting-for --image=nginx:1.19.6
+#kubectl wait --for condition=Ready --timeout 30s pod/a-pod-worth-waiting-for
+
+#kubectl apply -f ../yaml/a-deployment-worth-waiting-for.yml
+#kubectl wait --for condition=Available --timeout 60s deployment.apps/a-deployment-worth-waiting-for
+
+# NOTE: use 2 or 3 commands to show the success
+#kubectl get pods a-pod-worth-wating-for
+#kubectl get deployment.apps a-deployment-worth-waiting-for
