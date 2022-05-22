a16c3e40e0237982e8f6bb0642af6d856932d9ce
Stuart Feeser <sfeeser@gmail.com>
2022-05-22 16:41:59 -0400

    Update test.sh

--- labs/setup/horizontal-scaling-with-kubectl-scale/test.sh
+++ labs/setup/horizontal-scaling-with-kubectl-scale/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 kubectl apply -f ../yaml/sise-deploy.yaml
-kubectl wait --for condition=Ready --timeout 30s deployment/sise-deploy.yaml
+kubectl wait --for condition=Ready --timeout 30s deployment/sise-deploy
 kubectl scale deployment sise-deploy --replicas=3
 kubectl wait --for condition=Ready --timeout 30s deployment/sise-deploy
 kubectl apply -f ../yaml/webby-deploy.yaml
