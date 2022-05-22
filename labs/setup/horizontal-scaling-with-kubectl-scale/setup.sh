d552ed511fc8214ecf7b3325d244a3898139d64f
Stuart Feeser <sfeeser@gmail.com>
2022-05-22 16:19:49 -0400

    Update setup.sh

--- labs/setup/horizontal-scaling-with-kubectl-scale/setup.sh
+++ labs/setup/horizontal-scaling-with-kubectl-scale/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
-echo STARTING ----------
-kubectl get nodes || printf "\n\n Looks like K8s may not be installed?\n\n"
-echo END OF SETUP ----------
+kubectl apply -f ../yaml/sise-deploy.yaml
+kubectl wait --for condition=Ready --timeout 30s deployment/sise-deploy.yaml
+kubectl scale deployment sise-deploy --replicas=3
+kubectl wait --for condition=Ready --timeout 30s deployment/sise-deploy.yaml
+kubectl apply -f ../yaml/webby-deploy.yaml
+kubectl wait --for condition=Ready --timeout 30s deployment/webby-deploy.yaml
+kubectl get -f ../yaml/webby-deploy.yaml
+kubectl get -f ../yaml/sise-deploy.yaml
