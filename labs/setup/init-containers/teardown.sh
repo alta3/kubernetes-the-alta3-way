a9c8d97ac51b47901debfbf45e1f8158c49d9c48
bryfry <bryon.fryer@gmail.com>
2022-05-22 18:42:13 +0000

    s,t,t updates

--- labs/setup/init-containers/teardown.sh
+++ labs/setup/init-containers/teardown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "[-] No teardown"
-
+kubectl delete --ignore-not-found --wait=false \
+  -f ../yaml/init-cont-pod.yaml \
+  -f ../yaml/myservice.yaml \
+  -f ../yaml/mydb.yaml
--- labs/setup/init-containers/test.sh
+++ labs/setup/init-containers/test.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -xeuo pipefail
 
-
+kubectl apply -f ../yaml/init-cont-pod.yaml
+kubectl apply -f ../yaml/myservice.yaml
+kubectl apply -f ../yaml/mydb.yaml 
+kubectl wait --for condition=Ready --timeout 30s -f ../yaml/init-cont-pod.yaml
+kubectl get pods
