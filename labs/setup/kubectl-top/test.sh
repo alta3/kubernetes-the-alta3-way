dd943c8c1dadc19b3a405cab480d140ffa22a373
bryfry <bryon.fryer@gmail.com>
2022-05-22 14:52:26 +0000

    s,t,t updates

--- labs/setup/kubectl-top/test.sh
+++ labs/setup/kubectl-top/test.sh
@@ -2,6 +2,6 @@
 set -xeuo pipefail
 
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-kubectl -n kube-system wait --for condition=Available --timeout 60s deployment.apps/metrics-server
+kubectl -n kube-system wait --for condition=Available --timeout 100s deployment.apps/metrics-server
 kubectl top nodes
 kubectl top pods --all-namespaces
