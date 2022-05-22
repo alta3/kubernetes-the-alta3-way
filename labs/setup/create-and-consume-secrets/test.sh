6200a187395ae8a80744d7d373c926e69dc4b238
bryfry <bryon.fryer@gmail.com>
2022-05-22 19:11:11 +0000

    s,t,t updates

--- labs/setup/create-and-consume-secrets/test.sh
+++ labs/setup/create-and-consume-secrets/test.sh
@@ -4,5 +4,5 @@ set -xeuo pipefail
 kubectl apply -f ../yaml/mysql-secret.yaml
 kubectl apply -f ../yaml/mysql-locked.yaml
 kubectl wait --for condition=Ready --timeout 30s pod/mysql-locked
-kubectl get pod mysql-secret
+kubectl get pod mysql-locked
 kubectl get secrets mysql-secret
