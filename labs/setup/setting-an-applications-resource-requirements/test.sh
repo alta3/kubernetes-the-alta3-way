cd44748d45276b460a4c04fb6ac589f389a18dd4
bryfry <bryon.fryer@gmail.com>
2022-05-22 15:51:59 +0000

    s,t,t updates

--- labs/setup/setting-an-applications-resource-requirements/test.sh
+++ labs/setup/setting-an-applications-resource-requirements/test.sh
@@ -2,4 +2,7 @@
 set -xeuo pipefail 
 
 kubectl apply -f ../yaml/linux-pod-r.yaml
+kubectl wait --for condition=Ready --timeout 30s pod/linux-pod-r
 kubectl apply -f ../yaml/linux-pod-rl.yaml
+kubectl wait --for condition=Ready --timeout 30s pod/linux-pod-rl
+
