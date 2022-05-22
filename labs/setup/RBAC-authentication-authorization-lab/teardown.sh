68a12e0ee74dd95c9c95071c90af49c625952d2a
bryfry <bryon.fryer@gmail.com>
2022-05-22 21:38:34 +0000

    s,t,t updates

--- labs/setup/RBAC-authentication-authorization-lab/teardown.sh
+++ labs/setup/RBAC-authentication-authorization-lab/teardown.sh
@@ -5,3 +5,10 @@ kubectl delete --ignore-not-found --wait="${WAIT}" \
   -f ../yaml/prod-ns.yaml \
   -f ../yaml/test-ns.yaml \
   -f ../yaml/dev-ns.yaml
+
+kubectl delete --ignore-not-found --wait="${WAIT}" \
+  -f ../yaml/t3-support.yaml \
+  -f ../yaml/alice-csr.yaml \
+  -f ../yaml/t3-support-binding.yaml
+
+#kubectl certificate approve alice
--- labs/setup/RBAC-authentication-authorization-lab/test.sh
+++ labs/setup/RBAC-authentication-authorization-lab/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 
-
-
+kubectl apply -f ../yaml/t3-support.yaml
+kubectl apply -f ../yaml/alice-csr.yaml
+kubectl certificate approve alice
+kubectl apply -f ../yaml/t3-support-binding.yaml
