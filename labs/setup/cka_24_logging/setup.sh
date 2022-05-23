79713ac1ed75a2a7e49aa8dfd339325c508bffc1
bryfry <bryon.fryer@gmail.com>
2022-05-23 00:18:59 +0000

    s,t,t updates

--- labs/setup/cka_24_logging/setup.sh
+++ labs/setup/cka_24_logging/setup.sh
@@ -1,5 +1,2 @@
-#!/usr/bin/bash
-
-echo STARTING ----------
-kubectl get nodes || printf "\n\n Looks like K8s may not be installed?\n\n"
-echo END OF SETUP ----------
+#!/bin/bash
+set -euo pipefail
--- labs/setup/cka_24_logging/test.sh
+++ labs/setup/cka_24_logging/test.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -xeuo pipefail
 
-
+kubectl apply -f ../yaml/counter-pod.yaml
+kubectl wait --for condition=Ready --timeout 30s pod/counter
+kubectl apply -f ../yaml/two-pack.yaml
+kubectl wait --for condition=Available --timeout 30s deployment.apps/two-pack
+kubectl apply -f ../yaml/nginx-rsyslog-pod.yaml
