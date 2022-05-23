ff436ffed80ab4734bcc3c801237df1435f0f932
bryfry <bryon.fryer@gmail.com>
2022-05-23 02:34:32 +0000

    s,t,t updates

--- labs/setup/fluentd/teardown.sh
+++ labs/setup/fluentd/teardown.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "[-] No teardown"
-
+kubectl delete --ignore-not-found --wait="${WAIT}" \
+  -f ../yaml/fluentd-conf.yaml \
+  -f ../yaml/fluentd-pod.yaml \
+  -f ../yaml/http_fluentd_config.yaml \
+  -f ../yaml/http_fluentd.yaml 
--- labs/setup/fluentd/test.sh
+++ labs/setup/fluentd/test.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
+set -xeuo pipefail
 
+kubectl apply -f ../yaml/fluentd-conf.yaml 
+kubectl apply -f ../yaml/fluentd-pod.yaml
+kubectl wait --for condition=Ready --timeout 30s pod/logger
+BCHD_IP=$(hostname -f) j2 ../yaml/http_fluentd_config.yaml | kubectl apply -f - 
+kubectl apply -f ../yaml/http_fluentd.yaml 
+kubectl wait --for condition=Ready --timeout 30s pod/http-logger
+kubectl get pods
+
+# TODO test recieving logs
 
