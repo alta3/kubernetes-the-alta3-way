ec8a0b8a75bdf4064e38831df6144411d3f9f145
bryfry <bryon.fryer@gmail.com>
2022-05-23 02:59:25 +0000

    s,t,t updates

--- labs/setup/understanding-labels-and-selectors/test.sh
+++ labs/setup/understanding-labels-and-selectors/test.sh
@@ -2,6 +2,6 @@
 set -xeuo pipefail
 
 kubectl apply -f ../yaml/nginx-pod.yaml
-kubectl wait --for condition=Ready --timeout 30s -f pod/nginx
+kubectl wait --for condition=Ready --timeout 30s pod/nginx
 kubectl apply -f ../yaml/nginx-obj.yaml
-kubectl wait --for condition=Ready --timeout 30s -f pod/nginx
+kubectl wait --for condition=Available --timeout 30s deployment.apps/nginx-obj-create
