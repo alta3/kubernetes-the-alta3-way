de3f39e49fec1a1e0d0bb811d0024b7f4bfeea45
bryfry <bryon.fryer@gmail.com>
2022-05-23 00:28:15 +0000

    s,t,t updates

--- labs/setup/advanced-logging/test.sh
+++ labs/setup/advanced-logging/test.sh
@@ -6,3 +6,4 @@ kubectl wait --for condition=Ready --timeout 30s pod/counter
 kubectl apply -f ../yaml/two-pack.yaml
 kubectl wait --for condition=Available --timeout 30s deployment.apps/two-pack
 kubectl apply -f ../yaml/nginx-rsyslog-pod.yaml
+kubectl wait --for condition=Ready --timeout 30s pod/nginx-rsyslog-pod
