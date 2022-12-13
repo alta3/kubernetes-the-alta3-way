61256a4eb0abccc2b1e3a0f1dad6552e22d2d96f
bryfry <bryon.fryer@gmail.com>
2022-12-13 04:59:02 +0000

    add teardown for configmaps

--- labs/setup/persistent-configuration-with-configmaps/teardown.sh
+++ labs/setup/persistent-configuration-with-configmaps/teardown.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+kubectl delete configmap nginx-base-conf
+kubectl delete configmap index-html-zork
+kubectl delete configmap nineteen-eighty-four
 kubectl delete pod nginx-configured
