2c586c291b3c2a0d72993c4500e86a7b20bee2f0
bryfry <bryon.fryer@gmail.com>
2022-12-13 04:03:20 +0000

    fix source file paths

--- labs/setup/persistent-configuration-with-configmaps/test.sh
+++ labs/setup/persistent-configuration-with-configmaps/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-kubectl create configmap nginx-base-conf --from-file=nginx.conf=mycode/config/nginx-base.conf
-kubectl create configmap index-html-zork  --from-file=index.html=mycode/config/index-html-zork.html
-kubectl create configmap nineteen-eighty-four  --from-file=1984.txt=mycode/config/nineteen-eighty-four.txt
-kubectl apply -f ~/mycode/yaml/nginx-configured.yaml
+kubectl create configmap nginx-base-conf --from-file=nginx.conf=../config/nginx-base.conf
+kubectl create configmap index-html-zork  --from-file=index.html=../config/index-html-zork.html
+kubectl create configmap nineteen-eighty-four  --from-file=1984.txt=../config/nineteen-eighty-four.txt
+kubectl apply -f ../yaml/nginx-configured.yaml
 kubectl wait --for condition=Ready --timeout 30s pod/nginx-configured
 kubectl get pod nginx-configured
