70e3fdf8d71d114420a16e20c3114f8b9a71c733
seaneon <35880398+seaneon@users.noreply.github.com>
2023-03-31 11:53:33 -0400

    Update drill.sh

---
+++ labs/drill/application-monitoring/drill.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+set -eou pipeline
+echo STARTING ----------
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+CYAN='\033[0;36m'
+echo -e ${CYAN} "This one minute pause is intentional. It allows the metrics server to finish to completion.
+sleep 60
+kubectl apply -f ~/mycode/yaml/ctce-drill-monitoring.yaml
+echo END OF SETUP ----------
