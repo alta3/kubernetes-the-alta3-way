5881475e3a941537f858d526bf722f3ae28bc941
seaneon <35880398+seaneon@users.noreply.github.com>
2023-03-31 12:44:42 -0400

    Update drill.sh

--- labs/drill/application-monitoring/drill.sh
+++ labs/drill/application-monitoring/drill.sh
@@ -3,7 +3,7 @@
 echo STARTING ----------
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 CYAN='\033[0;36m'
-echo -e ${CYAN} "This one minute pause is intentional. It allows the metrics server to finish to completion.
+echo -e ${CYAN} "This one minute pause is intentional. It allows the metrics server to finish to completion."
 sleep 60
 kubectl apply -f ~/mycode/yaml/ctce-drill-monitoring.yaml
 echo END OF SETUP ----------
