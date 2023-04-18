c800b2c349f0331a38db22f8edd3ae60698241a1
seaneon <35880398+seaneon@users.noreply.github.com>
2023-04-18 11:41:14 -0400

    Update drill.sh

--- labs/drill/configmaps/drill.sh
+++ labs/drill/configmaps/drill.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
 echo STARTING ----------
-
+kubectl apply -f ~/mycode/yaml/ctce-drill-configmaps.yaml
+kubectl create configmap -n metallica nineteen-eighty-four --from-file=1984.txt=mycode/config/nineteen-eighty-four.txt
+kubectl create configmap -n metallica nginx-base-conf --from-file=nginx.conf=mycode/config/nginx-base.conf
 echo END OF SETUP ----------
