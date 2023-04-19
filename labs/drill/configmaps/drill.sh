f90050d701274a368b4c7c158d64197bf97efeec
seaneon <sean.r.barbour@gmail.com>
2023-04-19 16:52:28 -0400

    drill full path

--- labs/drill/configmaps/drill.sh
+++ labs/drill/configmaps/drill.sh
@@ -2,6 +2,6 @@
 
 echo STARTING ----------
 kubectl apply -f ~/mycode/yaml/ctce-drill-configmaps.yaml
-kubectl create configmap -n metallica nineteen-eighty-four --from-file=1984.txt=mycode/config/nineteen-eighty-four.txt
-kubectl create configmap -n metallica nginx-base-conf --from-file=nginx.conf=mycode/config/nginx-base.conf
+kubectl create configmap -n metallica nineteen-eighty-four --from-file=1984.txt=/home/student/mycode/config/nineteen-eighty-four.txt
+kubectl create configmap -n metallica nginx-base-conf --from-file=nginx.conf=/home/student/mycode/config/nginx-base.conf
 echo END OF SETUP ----------
