6560a06016642a5d227af486f6c60e4f4ccbc10f
seaneon <35880398+seaneon@users.noreply.github.com>
2023-04-19 15:08:58 -0400

    Update drill.sh

--- labs/drill/deploy-rollbacks/drill.sh
+++ labs/drill/deploy-rollbacks/drill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo STARTING ----------
-kubectl apply -f ~/mycode/yaml/ctce-drill-busted-deployment.yaml
+kubectl apply -f ~/mycode/yaml/ctce-drill-rollbacks.yaml
 echo END OF SETUP ----------
