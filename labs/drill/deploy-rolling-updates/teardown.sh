9af104aae7d8d5255624a58ea600647a37969040
seaneon <sean.r.barbour@gmail.com>
2023-04-20 11:34:45 -0400

    updating teardowns

--- labs/drill/deploy-rolling-updates/teardown.sh
+++ labs/drill/deploy-rolling-updates/teardown.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
-echo "[-] No teardown"
+kubectl delete --ignore-not-found --wait="${WAIT}" -f ~/mycode/yaml/ctce-drill-deployments.yaml
+kubectl delete --ignore-not-found --wait="${WAIT}" -f ~/manticore-deployment.yaml
+echo "Teardown complete"
