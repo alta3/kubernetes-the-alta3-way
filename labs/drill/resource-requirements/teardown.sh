567855e2b044bae0209a6b56ca098355ee14a66d
Chad Feeser <csfeeser@gmail.com>
2025-01-29 16:33:50 -0500

    script fix

--- labs/drill/resource-requirements/teardown.sh
+++ labs/drill/resource-requirements/teardown.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -e ~/zerotohero.yaml ]
-then
-  echo "deleting"
-  kubectl delete --ignore-not-found -f ~/zerotohero.yaml
-else
-  echo "file does not exist"
-fi
 
-kubectl delete --ignore-not-found -f ~/mycode/yaml/ctce-drill-resource-requirements.yaml
-kubectl delete --ignore-not-found -f ~/zerotohero.yaml
+kubectl delete pod zerotohero -n hercules --ignore-not-found --force --grace-period=0
+kubectl delete ns hercules --ignore-not-found
 
 echo "Teardown complete"
