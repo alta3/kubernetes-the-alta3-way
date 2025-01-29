917e9cff1bd64753b0fc990b13633ebeb30e0e0c
Chad Feeser <csfeeser@gmail.com>
2025-01-29 12:22:44 -0500

    fixes

--- labs/drill/cka-filtering-cluster-data/teardown.sh
+++ labs/drill/cka-filtering-cluster-data/teardown.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # teardown tasks go here
-kubectl delete -f https://static.alta3.com/courses/cka/exam/basenginx.yml --force --grace-period=0 &> /dev/null
-kubectl delete -f https://static.alta3.com/courses/cka/exam/lowcpunginx.yml --force --grace-period=0 &> /dev/null
-kubectl delete -f https://static.alta3.com/courses/cka/exam/highcpunginx.yml --force --grace-period=0 &> /dev/null
-kubectl delete ns integration &> /dev/null
+kubectl delete -f https://static.alta3.com/courses/cka/exam/basenginx.yml --force --grace-period=0 --ignore-not-found &> /dev/null
+kubectl delete -f https://static.alta3.com/courses/cka/exam/lowcpunginx.yml --force --grace-period=0 --ignore-not-found &> /dev/null
+kubectl delete -f https://static.alta3.com/courses/cka/exam/highcpunginx.yml --force --grace-period=0 --ignore-not-found &> /dev/null
+kubectl delete ns integration --force --grace-period=0 --ignore-not-found &> /dev/null
 
 echo "[+] Teardown complete! The task has been reset. You can now attempt again."
--- labs/drill/cka-persistent-volumes/teardown.sh
+++ labs/drill/cka-persistent-volumes/teardown.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 PV_NAME="rompv"
 
 # Force delete the PV and suppress output
-kubectl delete pv "$PV_NAME" --force --grace-period=0 &> /dev/null
+kubectl delete pv "$PV_NAME" --force --grace-period=0 --ignore-not-found &> /dev/null
 
 echo "[+] Teardown complete! The task has been reset. You can now attempt again."
