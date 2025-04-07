fc098010f86f9622c0df4d67fa691a265425e250
BicycleWalrus <32198667+BicycleWalrus@users.noreply.github.com>
2025-04-07 12:55:01 -0400

    Update and rename network-troubleshooting.bash to drill.sh

--- labs/drill/network-troubleshooting/drill.sh
+++ labs/drill/network-troubleshooting/drill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo STARTING ----------
-kubectl apply -f ~/mycode/yaml/ctce-drill-api-primitives.yaml
+kubectl apply -f ~/mycode/yaml/ctce-drill-network-troubleshooting.yaml
 echo END OF SETUP ----------
