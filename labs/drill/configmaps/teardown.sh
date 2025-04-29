4069242239a1608a6984d084f68d1d98fa3b4f64
seaneon <35880398+seaneon@users.noreply.github.com>
2025-04-29 12:10:05 -0400

    Update teardown.sh

--- labs/drill/configmaps/teardown.sh
+++ labs/drill/configmaps/teardown.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 kubectl delete --ignore-not-found -f ~/mycode/yaml/ctce-drill-configmaps.yaml
 kubectl delete --ignore-not-found -f ~/mycode/yaml/ctce-answers-configmaps.yaml
 kubectl delete --ignore-not-found -f ~/mycode/yaml/enter-sandman.yaml
-kubectl delete --ignore-not-found -f ~/mycode/yaml/metal.html
+#kubectl delete --ignore-not-found -f ~/mycode/yaml/metal.html << Not a yaml file. Can stay
 kubectl delete --ignore-not-found configmap -n metallica nineteen-eighty-four
 kubectl delete --ignore-not-found configmap -n metallica nginx-base-conf
 echo "Teardown complete"
